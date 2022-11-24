75b511e5ae0f4122ce44ba05dd13b54d3064448c
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2022-11-24 22:07:38 +0300

    Вынесен ModalForm

---
+++ src/components/ModalForm/ModalForm.tsx
@@ -0,0 +1,63 @@
+import React, { useState, ReactElement, Fragment } from 'react';
+import { Box, Modal, Grid, Button } from '@mui/material';
+
+const ModalForm = ({
+  textButton,
+  content,
+  saved,
+}: {
+  textButton: string;
+  content: ReactElement;
+  saved: any;
+}) => {
+  const [open, setOpen] = useState(false);
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #eee',
+    boxShadow: 2,
+    pt: 2,
+    px: 4,
+    pb: 3,
+  };
+
+  return (
+    <Fragment>
+      <Button onClick={() => setOpen(true)}>{textButton}</Button>
+
+      <Modal
+        hideBackdrop
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          {content}
+
+          <Grid container sx={{ marginTop: '10px' }}>
+            <Grid item xs={12} md={6}>
+              <Button onClick={() => setOpen(false)}>Отменить</Button>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Button
+                onClick={() => {
+                  setOpen(false);
+                  saved();
+                }}
+              >
+                Сохранить
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Modal>
+    </Fragment>
+  );
+};
+
+export default ModalForm;
--- src/pages/PageListProjects/PageListProjects.tsx
+++ src/pages/PageListProjects/PageListProjects.tsx
@@ -8,26 +8,12 @@ import { MapDispatchToProps } from '../../redux/services/MapDispatchToProps';
 import { Props } from './interfaces';
 import './PageListProjects.scss';
 
-import { Box, Button, Modal, Input, Grid } from '@mui/material';
+import { Input } from '@mui/material';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
+import ModalForm from '../../components/ModalForm/ModalForm';
 
 const PageListProjects = ({ projects, projectsLoaded, currentProjectUpdated }: Props) => {
   const [nameNewProject, setNameNewProject] = useState('');
-
-  const [open, setOpen] = useState(false);
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 800,
-    bgcolor: 'background.paper',
-    border: '2px solid #eee',
-    boxShadow: 2,
-    pt: 2,
-    px: 4,
-    pb: 3,
-  };
 
   const savedProject = () => {
     const newList = [...projects, { id: Date.now(), name: nameNewProject, tasks: [] }];
@@ -45,35 +31,11 @@ const PageListProjects = ({ projects, projectsLoaded, currentProjectUpdated }: P
         </Link>
       ))}
 
-      <button onClick={() => setOpen(true)}>Добавить проект</button>
-
-      <Modal
-        hideBackdrop
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="child-modal-title"
-        aria-describedby="child-modal-description"
-      >
-        <Box sx={{ ...style, width: 400 }}>
-          <Input placeholder="Наименование" onChange={(e) => setNameNewProject(e.target.value)} />
-
-          <Grid container sx={{ marginTop: '10px' }}>
-            <Grid item xs={12} md={6}>
-              <Button onClick={() => setOpen(false)}>Отменить</Button>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Button
-                onClick={() => {
-                  setOpen(false);
-                  savedProject();
-                }}
-              >
-                Сохранить
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </Modal>
+      <ModalForm
+        textButton="Добавить проект"
+        content={<Input placeholder="Наименование" onChange={(e) => setNameNewProject(e.target.value)} />}
+        saved={() => savedProject()}
+      />
     </div>
   );
 };
