49ad1725c6171a0db24fa49487faae965af3639a
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2022-11-24 16:54:34 +0300

    Подготовка проекта

---
+++ src/components/App/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PageListProjects from '../pages/PageListProjects/Page-ListProjects';
+import './App.scss';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <div className="app-container">
+          <div className="app-content">
+            <Routes>
+              <Route path="/" element={<PageListProjects />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
