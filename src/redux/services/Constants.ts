1b42eb0c4bebe22731efe463752892ecea8b9328
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2022-11-28 22:52:17 +0300

    enum Statutes

--- src/redux/services/Constants.ts
+++ src/redux/services/Constants.ts
@@ -1,3 +1,9 @@
 export const StatutesTexts = ['Queue', 'Development', 'Done'];
 
 export const PriorityTexts = ['Низкий', 'Средний', 'Высокий'];
+
+export enum Statutes {
+  Queue = 'Queue',
+  Development = 'Development',
+  Done = 'Done',
+}
